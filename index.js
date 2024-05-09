39d7a63ae511d44101a02d15cbccf521bb165733
vanlinh12356 <azaelcobospun0suy@hotmail.com>
2024-05-10 06:13:46 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const fibonacciRecursive = (n) => {
-  if (n <= 1) {
-    return n;
+function canFinish(numCourses, prerequisites) {
+  const graph = new Map();
+  const visited = new Array(numCourses).fill(0);
+  for (const [course, prerequisite] of prerequisites) {
+    if (!graph.has(course)) graph.set(course, []);
+    graph.get(course).push(prerequisite);
   }
-  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
-};
+  for (let i = 0; i < numCourses; i++) {
+    if (!dfs(i)) return false;
+  }
+  return true;
+  function dfs(course) {
+    if (visited[course] === 1) return false;
+    if (visited[course] === -1) return true;
+    visited[course] = 1;
+    if (graph.has(course)) {
+      for (const prerequisite of graph.get(course)) {
+        if (!dfs(prerequisite)) return false;
+      }
+    }
+    visited[course] = -1;
+    return true;
+  }
+}
